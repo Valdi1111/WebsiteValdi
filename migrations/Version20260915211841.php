55db1d223eaa709fedbcfc0d84437bfdbaf7ac44
Davide Valdifiori <davide.valdifiori.dv@gmail.com>
2026-09-16 23:57:27 +0200

    Hoyoverse: - Rename HoyoverseDiaryEntry index

--- migrations/Version20260915211841.php
+++ migrations/Version20260915211841.php
@@ -20,7 +20,7 @@ final class Version20260915211841 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE hoyoverse_diary_entry (id INT AUTO_INCREMENT NOT NULL, period VARCHAR(7) NOT NULL, currency VARCHAR(50) NOT NULL, recorded_at DATETIME NOT NULL, amount INT NOT NULL, action_key VARCHAR(64) DEFAULT NULL, action_name VARCHAR(128) DEFAULT NULL, hoyoverse_game_profile_id INT NOT NULL, INDEX IDX_7BB80FF37E3A7CB (hoyoverse_game_profile_id), INDEX IDX_hoyoverse_game_profile_id_period_currency (hoyoverse_game_profile_id, period, currency), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE hoyoverse_diary_entry (id INT AUTO_INCREMENT NOT NULL, period VARCHAR(7) NOT NULL, currency VARCHAR(50) NOT NULL, recorded_at DATETIME NOT NULL, amount INT NOT NULL, action_key VARCHAR(64) DEFAULT NULL, action_name VARCHAR(128) DEFAULT NULL, hoyoverse_game_profile_id INT NOT NULL, INDEX FK_hoyoverse_diary_entry_hoyoverse_game_profile (hoyoverse_game_profile_id), INDEX IDX_hoyoverse_game_profile_id_period_currency (hoyoverse_game_profile_id, period, currency), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('ALTER TABLE hoyoverse_diary_entry ADD CONSTRAINT FK_hoyoverse_diary_entry_hoyoverse_game_profile FOREIGN KEY (hoyoverse_game_profile_id) REFERENCES hoyoverse_game_profile (id) ON DELETE CASCADE');
     }
 
